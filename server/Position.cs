c461b6a7ca56d15df2e3666c43ed615d6a8b2910
Bartosz Starczewski <alejary@gmail.com>
2021-10-17 20:31:12 +0200

    added graphQl

---
+++ server/Position.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace server
+{
+    public class Position
+    {
+        public string StockSymbol { get; set; }
+        public int Units { get; set; }
+    }
+}
---
+++ server/graphQL/Query.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace server.graphQL
+{
+    public class Query
+    {
+        public Portfolio GetPortfolio() =>
+            new Portfolio {
+                Id = Guid.NewGuid(),
+                Name = "BS-long",
+                Positions = new[] {
+                    new Position
+                    {
+                        StockSymbol = "MSFT",
+                        Units = 10
+                    } 
+                }
+            };
+    }
+}
